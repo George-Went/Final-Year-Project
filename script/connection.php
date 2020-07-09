bb1d97b134f43348d2a28604ecd8bef2901c52d2
George-Went <gep.went@gmail.com>
2020-07-09 19:06:19 +0100

    Update connection.php

--- script/connection.php
+++ script/connection.php
@@ -1,7 +1,7 @@
 <?php 
    $servername = "localhost";
-   $username = "root";
-   $password = "password";
+   $username = "";
+   $password = "";
    $database = "hovercraft";
    $port = "3306";
    $connect = mysqli_connect($servername, $username, $password, $database, $port);
